d5aea254b02d5dc36d68b7aa6d7be744ebcbbced
chengxinyong <chengxinyong@139.com>
2018-09-06 11:34:57 +0800

    样式调整

--- js/getPagesSource.js
+++ js/getPagesSource.js
@@ -1,22 +1,22 @@
 var log = console.log.bind(console)
-
+log('goooooooooooooooo')
 function DOMtoString() {
     var html = document.querySelector('html').innerHTML
-    DOMadd();
     return html;
 }
 
-
 function DOMadd() {
     var body = document.querySelector('body')
-    var _old = document.querySelector('.page-finder-add')
+    var _old = document.querySelector('.page-finder-add-wrap')
     if (_old) {
         _old.remove()
     }
 
+    var _addWrap = document.createElement('div')
+    _addWrap.classList.add('page-finder-add-wrap')
     var _add = document.createElement('div')
     _add.classList.add('page-finder-add')
-    _add.setAttribute('style', 'z-index: 9999; box-sizing: border-box; position:fixed; max-width:50%; display: flex; flex-wrap: wrap; right: 10px; bottom: 10px; border: 1px solid #ccc; background: #278; padding: 5px')
+    _addWrap.appendChild(_add)
     
     // 读取图片
     var imgs = document.querySelectorAll('img')
@@ -27,11 +27,9 @@ function DOMadd() {
             flag = true
             var _div = document.createElement('div')
             _div.classList.add('page-img')
-            _div.setAttribute('style', 'padding:5px; height: 64px; width: auto; overflow: hidden')
     
             var _img = document.createElement('img')
             _img.setAttribute('src', src)
-            _img.setAttribute('style', 'height: 100%; background: #fff')
             _div.appendChild(_img)
     
             _add.appendChild(_div)
@@ -39,12 +37,43 @@ function DOMadd() {
     })
     
     if(flag){
-        body.appendChild(_add)
+        // 删除按钮
+        var operation = document.createElement('div')
+        operation.classList.add('page-finder-operation')
+        operation.innerText = '✕'
+        _add.appendChild(operation)
+       
+
+        // style 代码块
+        var _style = document.createElement('style')
+
+        var style = '.page-finder-add-wrap {' + 
+            'z-index: 9999; box-sizing: border-box; position:fixed; width:auto; right: 10px; bottom: 10px;' + 
+            'border: 1px solid #ccc; background: #278; padding: 5px; max-height: 35%; max-width:50%; display: flex; box-shadow: 4px 3px 10px #aaa' + 
+            '}\n'
+        style += '.page-finder-add {' + 
+            'display: flex; flex-wrap: wrap; overflow: auto ' + 
+            '}\n'
+        style += '.page-finder-operation {' +
+            'position: absolute; top: 0; right: 0; width: 30px; height: 30px; background: #fff; text-align: center;' +
+            'font-size: 20px; line-height: 30px; opacity: .85; cursor: pointer' +
+            '}\n'
+        style += '.page-finder-add .page-img {' +
+            'padding:5px; height: 64px; width: auto; max-width: 192px; overflow: hidden' + 
+            '}\n'
+        style += '.page-finder-add .page-img img {' +
+            'height: 100%; background: #fff' + 
+            '}\n'
+
+        _style.innerText = style
+        _addWrap.appendChild(_style)
+
+        body.appendChild(_addWrap)
+
+        operation.addEventListener('click', function(){
+            _addWrap.remove()
+        } , false)
     }
 }
 
-chrome.runtime.sendMessage({
-    action: "getSource",
-    source: DOMtoString()
-});
-
+DOMadd()
